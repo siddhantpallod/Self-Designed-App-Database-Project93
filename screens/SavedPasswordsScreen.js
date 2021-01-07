05e9ccbfa8a990ed105017e8eb7509738feeb18e
siddhantpallod <68698846+siddhantpallod@users.noreply.github.com>
2021-01-07 11:46:39 +0530

    Added Login Screen

---
+++ screens/SavedPasswordsScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class SavedPasswordsScreen extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Saved Passwords </Text>
+            </View>
+        )
+    }
+}
